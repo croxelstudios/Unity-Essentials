3d5db64745f5f6457c628fcdf0f3926098f02dff
HypercubeCore <38572928+HypercubeCore@users.noreply.github.com>
2024-08-07 21:54:14 +0200

    FindWithTag.InChildren

--- Systems/UnityAddons/FindWithTag.cs
+++ Systems/UnityAddons/FindWithTag.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using System.Linq;
 
 public static class FindWithTag
 {
@@ -246,6 +247,182 @@ public static class FindWithTag
         List<T> list = new List<T>();
         list.AddRange(OnlyEnableds<T>(new string[] { tag }));
         list.AddRange(OnlyEnableds<T>(extraTags));
+        return list.ToArray();
+    }
+    #endregion
+
+    #region In children
+    public static GameObject GameObjectInChildren(GameObject obj, params string[] tags)
+    {
+        return TransformInChildren(obj, tags).gameObject;
+    }
+
+    public static GameObject[] GameObjectsInChildren(GameObject obj, params string[] tags)
+    {
+        List<GameObject> list = new List<GameObject>();
+
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (tags.Contains(children[i].tag))
+                list.Add(children[i].gameObject);
+        }
+        return list.ToArray();
+    }
+
+    public static GameObject GameObjectInChildren(GameObject obj, params string[][] tags)
+    {
+        GameObject result = null;
+        if (tags != null)
+            for (int i = 0; i < tags.Length; i++)
+            {
+                result = GameObjectInChildren(obj, tags[i]);
+                if (result != null) break;
+            }
+        return result;
+    }
+
+    public static GameObject[] GameObjectsInChildren(GameObject obj, params string[][] tags)
+    {
+        List<GameObject> list = new List<GameObject>();
+        if (tags != null)
+            for (int i = 0; i < tags.Length; i++)
+                list.AddRange(GameObjectsInChildren(obj, tags[i]));
+        return list.ToArray();
+    }
+
+    public static GameObject GameObjectInChildren(GameObject obj, string tag, params string[] extraTags)
+    {
+        GameObject result = GameObject(new string[] { tag });
+        if (result == null) result = GameObjectInChildren(obj, extraTags);
+        return result;
+    }
+
+    public static GameObject[] GameObjectsInChildren(GameObject obj, string tag, params string[] extraTags)
+    {
+        List<GameObject> list = new List<GameObject>();
+        list.AddRange(GameObjectsInChildren(obj, new string[] { tag }));
+        list.AddRange(GameObjectsInChildren(obj, extraTags));
+        return list.ToArray();
+    }
+
+    public static Transform TransformInChildren(GameObject obj, params string[] tags)
+    {
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+        for (int i = 0;i < children.Length; i++)
+        {
+            if (tags.Contains(children[i].tag))
+                return children[i];
+        }
+        return null;
+    }
+
+    public static Transform[] TransformsInChildren(GameObject obj, params string[] tags)
+    {
+        List<Transform> list = new List<Transform>();
+
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (tags.Contains(children[i].tag))
+                list.Add(children[i]);
+        }
+        return list.ToArray();
+    }
+
+    public static Transform TransformInChildren(GameObject obj, params string[][] tags)
+    {
+        Transform result = null;
+        if (tags != null)
+            for (int i = 0; i < tags.Length; i++)
+            {
+                result = TransformInChildren(obj, tags[i]);
+                if (result != null) break;
+            }
+        return result;
+    }
+
+    public static Transform[] TransformsInChildren(GameObject obj, params string[][] tags)
+    {
+        List<Transform> list = new List<Transform>();
+        if (tags != null)
+            for (int i = 0; i < tags.Length; i++)
+                list.AddRange(TransformsInChildren(obj, tags[i]));
+        return list.ToArray();
+    }
+
+    public static Transform TransformInChildren(GameObject obj, string tag, params string[] extraTags)
+    {
+        Transform result = TransformInChildren(obj, new string[] { tag });
+        if (result == null) result = TransformInChildren(obj, extraTags);
+        return result;
+    }
+
+    public static Transform[] TransformsInChildren(GameObject obj, string tag, params string[] extraTags)
+    {
+        List<Transform> list = new List<Transform>();
+        list.AddRange(TransformsInChildren(obj, new string[] { tag }));
+        list.AddRange(TransformsInChildren(obj, extraTags));
+        return list.ToArray();
+    }
+
+    public static T ComponentInChildren<T>(GameObject obj, params string[] tags) where T : Component
+    {
+        T[] children = obj.GetComponentsInChildren<T>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (tags.Contains(children[i].tag))
+                return children[i];
+        }
+        return null;
+    }
+
+    public static T[] ComponentsInChildren<T>(GameObject obj, params string[] tags) where T : Component
+    {
+        List<T> list = new List<T>();
+
+        T[] children = obj.GetComponentsInChildren<T>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (tags.Contains(children[i].tag))
+                list.Add(children[i]);
+        }
+        return list.ToArray();
+    }
+
+    public static T ComponentInChildren<T>(GameObject obj, params string[][] tags) where T : Component
+    {
+        T result = null;
+        if (tags != null)
+            for (int i = 0; i < tags.Length; i++)
+            {
+                result = ComponentInChildren<T>(obj, tags[i]);
+                if (result != null) break;
+            }
+        return result;
+    }
+
+    public static T[] ComponentsInChildren<T>(GameObject obj, params string[][] tags) where T : Component
+    {
+        List<T> list = new List<T>();
+        if (tags != null)
+            for (int i = 0; i < tags.Length; i++)
+                list.AddRange(ComponentsInChildren<T>(obj, tags[i]));
+        return list.ToArray();
+    }
+
+    public static T ComponentInChildren<T>(GameObject obj, string tag, params string[] extraTags) where T : Component
+    {
+        T result = ComponentInChildren<T>(obj, new string[] { tag });
+        if (result == null) result = ComponentInChildren<T>(obj, extraTags);
+        return result;
+    }
+
+    public static T[] ComponentsInChildren<T>(GameObject obj, string tag, params string[] extraTags) where T : Component
+    {
+        List<T> list = new List<T>();
+        list.AddRange(ComponentsInChildren<T>(obj, new string[] { tag }));
+        list.AddRange(ComponentsInChildren<T>(obj, extraTags));
         return list.ToArray();
     }
     #endregion
