32fcbd0d343872868e74c6e22dbb3c329dd8017d
HypercubeCore <38572928+HypercubeCore@users.noreply.github.com>
2025-12-10 11:43:14 +0100

    Fixed issue with update of SetRenderingLayer

--- Systems/RendererRelatedCode/SetRenderingLayer.cs
+++ Systems/RendererRelatedCode/SetRenderingLayer.cs
@@ -1,47 +1,41 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class SetRenderingLayer : MonoBehaviour
 {
     [SerializeField]
-    LightLayerEnum renderingLayer = LightLayerEnum.LightLayerDefault;
+    RenderingLayerMask renderingLayers = RenderingLayerMask.defaultRenderingLayerMask;
 
     Renderer[] renderers;
 
     void OnEnable()
     {
-        System.Array enumValues = System.Enum.GetValues(typeof(LightLayerEnum));
         renderers = GetComponentsInChildren<Renderer>(true);
         for (int i = 0; i < renderers.Length; i++)
-        {
-            int bitmask = (int)renderingLayer;
-            bitmask = CalculateBitmask(bitmask, enumValues);
-            renderers[i].renderingLayerMask = (uint)bitmask;
-        }
+            renderers[i].renderingLayerMask = renderingLayers;
     }
 
-    int CalculateBitmask(int currentBitmask, System.Array enumValues)
-    {
-        foreach (LightLayerEnum current in enumValues)
-        {
-            if (current == LightLayerEnum.Everything) continue;
+    //int CalculateBitmask(int currentBitmask, System.Array enumValues)
+    //{
+    //    foreach (LightLayerEnum current in enumValues)
+    //    {
+    //        if (current == LightLayerEnum.Everything) continue;
 
-            int layerBitVal = (int)current;
+    //        int layerBitVal = (int)current;
 
-            bool set = current == renderingLayer;
-            currentBitmask = SetBitmask(currentBitmask, layerBitVal, set);
-        }
+    //        bool set = current == renderingLayer;
+    //        currentBitmask = SetBitmask(currentBitmask, layerBitVal, set);
+    //    }
 
-        return currentBitmask;
-    }
+    //    return currentBitmask;
+    //}
 
-    int SetBitmask(int bitmask, int bitVal, bool set)
-    {
-        if (set)
-            bitmask |= bitVal;
-        else
-            bitmask &= ~bitVal;
+    //int SetBitmask(int bitmask, int bitVal, bool set)
+    //{
+    //    if (set)
+    //        bitmask |= bitVal;
+    //    else
+    //        bitmask &= ~bitVal;
 
-        return bitmask;
-    }
+    //    return bitmask;
+    //}
 }
