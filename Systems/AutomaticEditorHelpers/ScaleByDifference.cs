67759b53fe2821b506e19dedbb963693305c3786
HypercubeCore <38572928+HypercubeCore@users.noreply.github.com>
2026-01-21 19:38:51 +0100

    Expanded functionality of scaleByDifference

--- Systems/AutomaticEditorHelpers/ScaleByDifference.cs
+++ Systems/AutomaticEditorHelpers/ScaleByDifference.cs
@@ -10,26 +10,28 @@ public class ScaleByDifference : MonoBehaviour
     [SerializeField]
     Vector3 byAxis = Vector3.one;
     [SerializeField]
+    AxisBooleans useAxis = new AxisBooleans(true, true, true);
+    [SerializeField]
     bool runtime = false;
 
     void Update()
     {
         if (runtime
-    #if UNITY_EDITOR
+#if UNITY_EDITOR
             || (!Application.isPlaying)
-    #endif
+#endif
             )
-            {
-                Vector3 target = ((other != null) ? other.lossyScale : Vector3.one)
-                    - (byAxis * difference);
-                Vector3 scl = target.InverseScale(transform.lossyScale);
-                Vector3 res = transform.localScale;
-                res.Scale(scl);
-                res.x = Mathf.Max(res.x, 0.0001f);
-                res.y = Mathf.Max(res.y, 0.0001f);
-                res.z = Mathf.Max(res.z, 0.0001f);
-                if (scl.sqrMagnitude.IsBetween(float.NegativeInfinity, float.PositiveInfinity))
-                    transform.localScale = res;
-            }
+        {
+            Vector3 target = ((other != null) ? other.lossyScale : Vector3.one)
+                - (byAxis * difference);
+            Vector3 scl = target.InverseScale(transform.lossyScale);
+            Vector3 res = transform.localScale;
+            res.Scale(scl);
+            res.x = useAxis.x ? Mathf.Max(res.x, 0.0001f) : transform.localScale.x;
+            res.y = useAxis.y ? Mathf.Max(res.y, 0.0001f) : transform.localScale.y;
+            res.z = useAxis.z ? Mathf.Max(res.z, 0.0001f) : transform.localScale.z;
+            if (scl.sqrMagnitude.IsBetween(float.NegativeInfinity, float.PositiveInfinity))
+                transform.localScale = res;
+        }
     }
 }
