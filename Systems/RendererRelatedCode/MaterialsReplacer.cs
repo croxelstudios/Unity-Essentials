e5c38cafed8de8653d8024e5ffd606a6bf347166
HypercubeCore <38572928+HypercubeCore@users.noreply.github.com>
2026-02-24 01:10:24 +0100

    Fixed MaterialsReplacer I think

--- Systems/RendererRelatedCode/MaterialsReplacer.cs
+++ Systems/RendererRelatedCode/MaterialsReplacer.cs
@@ -3,10 +3,11 @@ using UnityEngine.Rendering;
 using System.Collections.Generic;
 using System;
 using Sirenix.OdinInspector;
+using uParser;
+
 #if UNITY_EDITOR
-using UnityEngine.SceneManagement;
-using UnityEditor.SceneManagement;
-# endif
+using UnityEditor;
+#endif
 
 [ExecuteAlways]
 public class MaterialsReplacer : MonoBehaviour
@@ -14,69 +15,50 @@ public class MaterialsReplacer : MonoBehaviour
     [SerializeField]
     int parentSearch = 0;
     [SerializeField]
+    [OnValueChanged("UpdateMaterials", true)]
     MaterialPair[] materialReplacements = null;
-    [SerializeField]
-    bool reverseReplacement = false;
-#if UNITY_EDITOR
-    [SerializeField]
-    [OnValueChanged("Restart")]
-    EditorMode editorMode = EditorMode.OnRendering;
-# endif
 
-    enum EditorMode { OnRendering, OnEnable, DontReplace }
-
-    Renderer[] rend;
+    RendererData[] rend;
     Material[] tmpMaterials;
     Dictionary<RendMat, Material> changedMaterials;
     Dictionary<Material, Material> replacements;
-    Dictionary<Material, Material> inverseReplacements;
     bool isChanged;
+
+    Renderer[] instanced;
 
     void UpdateMaterials()
     {
         replacements = replacements.ClearOrCreate();
-        inverseReplacements = inverseReplacements.ClearOrCreate();
         foreach (MaterialPair m in materialReplacements)
-        {
-            if (replacements.ContainsKey(m.replaceThis))
-                replacements.Remove(m.replaceThis);
             replacements.Add(m.replaceThis, m.byReplacement);
-            if (inverseReplacements.ContainsKey(m.byReplacement))
-                inverseReplacements.Remove(m.byReplacement);
-            inverseReplacements.Add(m.byReplacement, m.replaceThis);
-        }
     }
 
     public void UpdateRenderers()
     {
+        RemoveInstances();
         Transform parent = transform;
         for (int i = 0; i < parentSearch; i++)
             parent = parent.parent;
-        rend = parent.GetComponentsInChildren<Renderer>(true);
+        Renderer[] r = parent.GetComponentsInChildren<Renderer>(true);
+        rend = new RendererData[r.Length];
+        for (int i = 0; i < rend.Length; i++)
+            rend[i] = new RendererData(r[i]);
     }
 
     void OnEnable()
     {
+        UpdateMaterials();
         UpdateRenderers();
 
 #if UNITY_EDITOR
         if (!Application.isPlaying)
         {
-            switch (editorMode)
-            {
-                case EditorMode.OnRendering:
-                    RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
-                    RenderPipelineManager.endCameraRendering += OnEndCameraRendering;
-                    break;
-                case EditorMode.OnEnable:
-                    ReplaceMaterials();
-                    break;
-                default:
-                    return;
-            }
+            RenderPipelineManager.beginContextRendering += OnBeginContextRendering;
+            RenderPipelineManager.endContextRendering += OnEndContextRendering;
+            Undo.postprocessModifications += OnPostprocess;
         }
         else
-# endif
+#endif
             ReplaceMaterials();
     }
 
@@ -85,28 +67,109 @@ public class MaterialsReplacer : MonoBehaviour
 #if UNITY_EDITOR
         if (!Application.isPlaying)
         {
-            RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
-            RenderPipelineManager.endCameraRendering -= OnEndCameraRendering;
-        }
-#endif
-        ResetMaterials();
-    }
+            RenderPipelineManager.beginContextRendering -= OnBeginContextRendering;
+            RenderPipelineManager.endContextRendering -= OnEndContextRendering;
+            Undo.postprocessModifications -= OnPostprocess;
 
-    public void Restart()
-    {
-        OnDisable();
-        OnEnable();
+            EditorApplication.delayCall += () => RemoveInstances();
+        }
+        else
+#endif
+            ResetMaterials();
     }
 
 #if UNITY_EDITOR
-    void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
+    void Update()
     {
-        ReplaceMaterials();
+        bool updateInstances = false;
+        for (int i = 0; i < rend.Length; i++)
+        {
+            if (rend[i].IsNull())
+            {
+                UpdateRenderers();
+                updateInstances = true;
+                break;
+            }
+            rend[i].UpdateActiveState();
+        }
+
+        if ((!updateInstances) && (instanced.IsNullOrEmpty() || (instanced.Length != rend.Length)))
+            updateInstances = true;
+
+        if (!updateInstances)
+        {
+            for (int i = 0; i < instanced.Length; i++)
+                if (instanced[i] == null)
+                {
+                    updateInstances = true;
+                    break;
+                }
+        }
+
+        if (updateInstances)
+            Reinstantiate();
     }
 
-    void OnEndCameraRendering(ScriptableRenderContext context, Camera camera)
+    UndoPropertyModification[] OnPostprocess(UndoPropertyModification[] modifications)
     {
-        ResetMaterials();
+        bool updateInstances = false;
+
+        foreach (UndoPropertyModification m in modifications)
+        {
+            if (m.currentValue.target is Renderer ren)
+            {
+                for (int i = 0; i < rend.Length; i++)
+                    if (rend[i].rend == ren)
+                    {
+                        if ((!instanced.IsNullOrEmpty()) && (instanced[i] != null))
+                        {
+                            instanced[i].GetCopyOf(rend[i].rend);
+                            ReplaceMaterials(instanced[i], false);
+                        }
+                        else updateInstances = true;
+                    }
+            }
+            else if (m.currentValue.target is MeshFilter filter)
+            {
+                for (int i = 0; i < rend.Length; i++)
+                    if (rend[i].gameObject == filter.gameObject)
+                    {
+                        if ((!instanced.IsNullOrEmpty()) && (instanced[i] != null))
+                        {
+                            MeshFilter fil = instanced[i].GetComponent<MeshFilter>();
+                            fil.GetCopyOf(filter);
+                        }
+                        else updateInstances = true;
+                    }
+            }
+            else if (m.currentValue.target is GameObject obj)
+            {
+                for (int i = 0; i < rend.Length; i++)
+                    if (rend[i].gameObject == obj)
+                        rend[i].UpdateActiveState();
+            }
+        }
+
+        if (updateInstances)
+            Reinstantiate();
+
+        return modifications;
+    }
+
+    void OnBeginContextRendering(ScriptableRenderContext context, List<Camera> cameras)
+    {
+        for (int i = 0; i < rend.Length; i++)
+            if (rend[i].enabled)
+                rend[i].rend.enabled = false;
+        //ReplaceMaterials();
+    }
+
+    void OnEndContextRendering(ScriptableRenderContext context, List<Camera> cameras)
+    {
+        for (int i = 0; i < rend.Length; i++)
+            if (rend[i].enabled)
+                rend[i].rend.enabled = true;
+        //ResetMaterials();
     }
 #endif
 
@@ -115,19 +178,9 @@ public class MaterialsReplacer : MonoBehaviour
         changedMaterials = changedMaterials.CreateIfNull();
         if (!isChanged)
         {
-            UpdateMaterials();
-            foreach (Renderer r in rend)
-                if (r != null)
-                {
-                    tmpMaterials = r.sharedMaterials;
-                    for (int i = 0; i < tmpMaterials.Length; i++)
-                        if (replacements.ContainsKey(tmpMaterials[i]))
-                        {
-                            changedMaterials.Add(new RendMat(r, i), tmpMaterials[i]);
-                            tmpMaterials[i] = replacements[tmpMaterials[i]];
-                        }
-                    r.sharedMaterials = tmpMaterials;
-                }
+            foreach (RendererData r in rend)
+                ReplaceMaterials(r.rend);
+
             isChanged = true;
         }
     }
@@ -136,20 +189,8 @@ public class MaterialsReplacer : MonoBehaviour
     {
         if (isChanged)
         {
-            if (reverseReplacement)
-            {
-                UpdateMaterials();
-                foreach (Renderer r in rend)
-                    if (r != null)
-                    {
-                        tmpMaterials = r.sharedMaterials;
-                        for (int i = 0; i < tmpMaterials.Length; i++)
-                            if (inverseReplacements.ContainsKey(tmpMaterials[i]))
-                                tmpMaterials[i] = inverseReplacements[tmpMaterials[i]];
-                        r.sharedMaterials = tmpMaterials;
-                    }
-            }
-            else foreach (KeyValuePair<RendMat, Material> kv in changedMaterials)
+            foreach (KeyValuePair<RendMat, Material> kv in changedMaterials)
+                if (kv.Key.rend != null)
                 {
                     Material[] sm = kv.Key.rend.sharedMaterials;
                     sm[kv.Key.mat] = kv.Value;
@@ -159,6 +200,62 @@ public class MaterialsReplacer : MonoBehaviour
             isChanged = false;
         }
     }
+
+    void ReplaceMaterials(Renderer renderer, bool register = true)
+    {
+        tmpMaterials = renderer.sharedMaterials;
+        for (int i = 0; i < tmpMaterials.Length; i++)
+            if ((tmpMaterials[i] != null) &&
+                replacements.TryGetValue(tmpMaterials[i], out Material replacement))
+            {
+                if (register)
+                    changedMaterials.Add(new RendMat(renderer, i), tmpMaterials[i]);
+                tmpMaterials[i] = replacement;
+            }
+        renderer.sharedMaterials = tmpMaterials;
+    }
+
+#if UNITY_EDITOR
+    void Reinstantiate()
+    {
+        RemoveInstances();
+        InstanceRenderersCopy();
+    }
+
+    void InstanceRenderersCopy()
+    {
+        instanced = new Renderer[rend.Length];
+        for (int i = 0; i < rend.Length; i++)
+            if (!rend[i].IsNull())
+            {
+                GameObject orig = rend[i].gameObject;
+                GameObject go = new GameObject(orig.name + "_InstancedCopy");
+                go.hideFlags = HideFlags.HideAndDontSave;
+                go.transform.SetParent(orig.transform, false);
+                go.layer = orig.layer;
+                go.tag = orig.tag;
+                go.isStatic = orig.isStatic;
+                MeshFilter origFilter = orig.GetComponent<MeshFilter>();
+                if (origFilter != null)
+                {
+                    MeshFilter filter = go.AddComponent<MeshFilter>();
+                    filter.GetCopyOf(origFilter);
+                }
+                instanced[i] = go.AddComponent(rend[i].rend.GetType()) as Renderer;
+                instanced[i].GetCopyOf(rend[i].rend);
+                ReplaceMaterials(instanced[i], false);
+            }
+    }
+
+    void RemoveInstances()
+    {
+        if (instanced != null)
+            for (int i = 0; i < instanced.Length; i++)
+                if (instanced[i] != null)
+                    instanced[i].gameObject.DestroyOrImmediate();
+        instanced = null;
+    }
+#endif
 
     [ContextMenu("Replace Permanently")]
     public void ReplacePermanently()
@@ -177,6 +274,42 @@ public class MaterialsReplacer : MonoBehaviour
         {
             this.replaceThis = replaceThis;
             this.byReplacement = byReplacement;
+        }
+    }
+
+    [Serializable]
+    struct RendererData
+    {
+        public Renderer rend;
+        public GameObject gameObject;
+        public bool enabled;
+
+        public RendererData(Renderer rend)
+        {
+            this.rend = rend;
+            gameObject = rend.gameObject;
+            enabled = rend.enabled;
+        }
+
+        public bool IsNull()
+        {
+            return rend == null;
+        }
+
+        public bool UpdateActiveState(bool state)
+        {
+            bool active = state;
+            if (enabled != active)
+            {
+                enabled = active;
+                return true;
+            }
+            else return false;
+        }
+
+        public bool UpdateActiveState()
+        {
+            return UpdateActiveState(rend.enabled);
         }
     }
 }
